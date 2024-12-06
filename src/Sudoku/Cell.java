98f380e78885e54a479a328de667e9cb6dc34271
Mohammad Ferdinand Valliandra <80774713+andravall@users.noreply.github.com>
2024-12-06 19:55:16 +0700

    Update Cell.java

--- src/Sudoku/Cell.java
+++ src/Sudoku/Cell.java
@@ -1,3 +1,19 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231159 - Mohammad Ferdinand Valliandra
+ * 2 - 5026231135 - Fachreza Aptadhi Kurniawan
+ * 3 - 5026231149 - Ananda Donelly Reksana
+ */
+
+package Sudoku;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class Cell extends JTextField {
     private static final long serialVersionUID = 1L;
     private int row, col;
