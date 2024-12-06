53486165844308200401f6e16ed2f7be01b7e4ef
Fachreza28 <162418737+Fachreza28@users.noreply.github.com>
2024-12-06 20:05:32 +0700

    Update Cell.java

--- src/Sudoku/Cell.java
+++ src/Sudoku/Cell.java
@@ -8,6 +8,16 @@
  * 3 - 5026231149 - Ananda Donelly Reksana
  */
 
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231159 - Mohammad Ferdinand Valliandra
+ * 2 - 5026231135 - Fachreza Aptadhi Kurniawan
+ * 3 - 5026231149 - Ananda Donelly Reksana
+ */
+
 package Sudoku;
 
 import java.awt.*;
@@ -111,3 +121,4 @@ public class Cell extends JTextField {
         }
     }
 }
+
